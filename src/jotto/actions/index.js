8205dd5a0cba1b6c8ef58aa0d3d4d48b627e4e91
trinathsamanta <trinathsamanta@gmail.com>
2021-06-16 23:04:58 +0530

    api call using moxios

---
+++ src/jotto/actions/index.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+export const getSecretWord = () => {
+  //return response from server
+  return axios.get('http://localhost:3030')
+  .then(response=>response.data);
+}
